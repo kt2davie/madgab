04bce2bc5c45d87e9fcefcd9c8065768530fbafe
Keefer Davies <keeferdavies@rogers.com>
2020-05-22 02:09:08 -0400

    Updates

---
+++ precache-manifest.1eda43cd0f18f7ec34cf335766691aa0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff98db29e81cd9308452fb6e902c34dc",
+    "url": "/madgab/index.html"
+  },
+  {
+    "revision": "1a13b747062ec659c89b",
+    "url": "/madgab/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "871f9d99229306aefdfe",
+    "url": "/madgab/static/css/main.0231d968.chunk.css"
+  },
+  {
+    "revision": "1a13b747062ec659c89b",
+    "url": "/madgab/static/js/2.64550796.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/madgab/static/js/2.64550796.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "871f9d99229306aefdfe",
+    "url": "/madgab/static/js/main.78078b02.chunk.js"
+  },
+  {
+    "revision": "0d4fb83ca30fea4b3214",
+    "url": "/madgab/static/js/runtime-main.81136b9b.js"
+  }
+]);
